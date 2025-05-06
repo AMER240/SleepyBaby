98fa8b3644cc0058a80e824e69920b650f25dbb7
Amir <amersheikhhader@gmail.com>
2025-05-06 03:44:47 +0300

    Enable SonarQube plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,11 @@ plugins {
     id("org.sonarqube") version "4.4.1.3373"
     alias(libs.plugins.android.application) apply false
 }
+sonarqube {
+    properties {
+        property("sonar.projectKey", "SleepyBaby")
+        property("sonar.organization", "amer240")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.login", System.getenv("SONAR_TOKEN"))
+    }
+}
