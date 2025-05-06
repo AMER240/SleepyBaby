02c4ebb97676168a1ab6091e397839bad70204b4
Amir <amersheikhhader@gmail.com>
2025-05-06 03:37:51 +0300

    SonarQube eklentisini kök projeye eklemek

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("org.sonarqube") version "4.4.1.3373"
     alias(libs.plugins.android.application) apply false
 }
