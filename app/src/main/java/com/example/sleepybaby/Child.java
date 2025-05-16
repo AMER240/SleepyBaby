fd278b63826266753d471aadf98697fde4a9803b
Muhamed Khuder <muhamedkhuder0@gmail.com>
2025-05-16 22:34:46 +0300

    Feature/front end (#20)
    * buton tasarımı duzenledim
    
    * thread layout ekledim
    
    * thread layout duzenledim
    
    * tasarimi deçistirdim
    
    ---------
    
    Co-authored-by: Amer <amersheikhhader@gmail.com>

---
+++ app/src/main/java/com/example/sleepybaby/Child.java
@@ -0,0 +1,56 @@
+package com.example.sleepybaby;
+
+public class Child {
+    private int id;
+    private String name;
+    private int age;
+    private int sleepHour;
+    private int sleepMinute;
+    private int wakeHour;
+    private int wakeMinute;
+
+    // Getter and Setter methods
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getSleepHour() {
+        return sleepHour;
+    }
+    public void setSleepHour(int sleepHour) {
+        this.sleepHour = sleepHour;
+    }
+    public int getSleepMinute() {
+        return sleepMinute;
+    }
+    public void setSleepMinute(int sleepMinute) {
+        this.sleepMinute = sleepMinute;
+    }
+    public int getWakeHour() {
+        return wakeHour;
+    }
+    public void setWakeHour(int wakeHour) {
+        this.wakeHour = wakeHour;
+    }
+    public int getWakeMinute() {
+        return wakeMinute;
+    }
+    public void setWakeMinute(int wakeMinute) {
+        this.wakeMinute = wakeMinute;
+    }
+}
--- app/src/main/java/com/example/sleepybaby/MainActivity.java
+++ app/src/main/java/com/example/sleepybaby/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sleepybaby;
 
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,14 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     TextView t;
@@ -27,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
             t=findViewById(R.id.tx);
