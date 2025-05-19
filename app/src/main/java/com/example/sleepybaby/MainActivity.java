402428e736032f76a8e9db06842ef53de144b924
Muhamed Khuder <muhamedkhuder0@gmail.com>
2025-05-19 16:29:49 +0300

    Feature/front end (#25)
    * issue yi halletim
    
    ---------
    
    Co-authored-by: Amer <amersheikhhader@gmail.com>

--- app/src/main/java/com/example/sleepybaby/MainActivity.java
+++ app/src/main/java/com/example/sleepybaby/MainActivity.java
@@ -8,8 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Button btnViewSchedule, btnAddChild;
+    Button btnViewSchedule;
+    Button btnAddChild;
 
 
     @Override
