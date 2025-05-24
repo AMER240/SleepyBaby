f51aa7b4ba15fd2f3e2465a49364521ddc5a5be2
Amer <amersheikhhader@gmail.com>
2025-05-24 23:19:04 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("org.sonarqube") version "4.4.1.3373"
-    alias(libs.plugins.android.application) apply false
+  id("org.sonarqube") version "6.2.0.5505"
 }
+
 sonar {
   properties {
     property("sonar.projectKey", "AMER240_SleepyBaby")
